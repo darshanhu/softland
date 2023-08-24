7fb9a8d789742ef175f8ec5fea84f27bf9ad5373
darshanhu <133042615+darshanhu@users.noreply.github.com>
2023-08-24 23:07:30 +0530

    Update init.js

--- pendo/init.js
+++ pendo/init.js
@@ -15,7 +15,7 @@
         {
             pendo.initialize({
             visitor: {
-                id:           'softland_user_33',
+                id:           'softland_user_33_disable',
                 email:        "jimmykimmel@softland.com",
                 full_name:    "Jimmy Kimmel",
                 first_name:   "Jimmy",
