a30a13a7b5d37f548c8b613af8c01eb700e95fb1
darshanhu <133042615+darshanhu@users.noreply.github.com>
2023-09-01 23:29:33 +0530

    Update init.js

--- pendo/init.js
+++ pendo/init.js
@@ -11,28 +11,4 @@
         // Please use Strings, Numbers, or Bools for value types.
         
     let database = "PRODUCTION"        
-    if(database  !== "DEMO")
-        {
-            pendo.initialize({
-            visitor: {
-                // id:           'softland_user_33_disabled_user',
-                email:        "jimmykimmel@softland.com",
-                full_name:    "Jimmy Kimmel",
-                first_name:   "Jimmy",
-                last_name:    "Kimmel"
-            },
-
-            account: {
-                id:           'SOFTLAND_ACCOUNT_1',
-                name:         "SOFTLAND_ACCOUNT",
-                creationDate: new Date().toISOString().slice(0, 10)
-            },
-            // disableGuides: (window.location.hostname === "softlandsite.netlify.app") ? true : false
-        });
-               console.log("Yay Admin is here");
-        } else {
-            console.log("YOu can do other work in this block");
-        }
-        
-
-})('');
+ )('43994d6b-66ea-49fc-71f1-31077e387150');
