f6a4f109e3778a6b427d34558adf12e7ab1cd89c
darshanhu <darshan.hulswar@pendo.io>
2023-09-22 03:21:01 +0530

    updated pendo

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.addEventListener('message', function(event) {
     console.log("Message received from the parent: " + event.data);
+    const btn = this.document.querySelector('#softland-btn');
+    btn.click();
     this.alert("HAHAHA") // Message received from parent
   });
